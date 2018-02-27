51ae72750ffa6284726395efe8b0621daf8083d6
AifiHenryMa <36319820+AifiHenryMa@users.noreply.github.com>
2018-02-27 18:01:35 +0800

    Update server.cpp
    更新服务器端程序

--- server.cpp
+++ server.cpp
@@ -1,10 +1,11 @@
-//for client
+// for server
 #include <iostream>
 #include <string>
 //for protobuf
-#include "protocolbufTest.pb.h" 
+#include "protocolbufTest.pb.h"
 //for zmq
 #include <zmq.h>
+#include <unistd.h>
 
 using namespace std;
 using namespace protocolbufTest::protobuf ;
@@ -13,43 +14,58 @@ const int BUFFSIZE = 128;
 
 int main()
 {
-    //socket通信所需的上下文环境
+    //使用ZMQ进行通信，首先要创建一个上下文环境，然后使用它创建套接字。
     void *context = zmq_ctx_new(); //create a context
     
-    //根据context建立的socket的链接，客户端使用ZMQ_REQ套接字
-    void *requester = zmq_socket(context, ZMQ_REQ); // for client
+    //根据context建立的socket的链接，服务端使用ZMQ_REP套接字
+    void *responder = zmq_socket(context, ZMQ_REP); // for server
     
-    //客户端要尝试连接到服务端提供的地址
-    if( -1 == zmq_connect(requester,"tcp://localhost:5555"))
+    //服务端将socket绑定到一个周知的地址和端口
+    if( -1 == zmq_bind(responder,"tcp://*:5555"))
     {
-        cout<<"Connect to server failed..."<<endl;
-        zmq_ctx_destroy(context);
+        cout<<"bind socket to server failed..."<<endl;
         return -1;
     }
-    cout<<"Connect to server success..."<<endl;
 
     Information myprotobuf;
     while(1)
     {
-        myprotobuf.set_type("client");
+        char buff[BUFFSIZE];
+        //接收客户端请求
+        //要把数据写入消息需要使用zmq_msg_init_size()来初始化消息
+        //而读取消息由于未知消息的长度只能使用zmq_msg_init()来创建一个空的消息。
+        zmq_msg_t request;
+        zmq_msg_init(&request);  //initialise empty 0MQ message
+        int size = zmq_msg_recv(&request,responder,0); //receive a message part from a socket
+        memcpy(buff,zmq_msg_data(&request),size); //zmq_msg_data - retrieve pointer to message content
+        Information receive;
+        receive.ParseFromArray(buff,BUFFSIZE); //Parse a protocol buffer contained in an array of bytes
+        cout<<"Type:"<<receive.type()<<"\t"
+            <<"IP:"<<receive.ip()<<"\t"
+            <<"Port:"<<receive.port()<<"\n";
+        zmq_msg_close(&request); //release 0MQ message
+
+        //清空接收缓存
+        memset(buff,0,BUFFSIZE*sizeof(char));
+        sleep(2); //停2秒
+
+        myprotobuf.set_type("server");
         myprotobuf.set_ip("192.168.1.100");
         myprotobuf.set_port(5555);
-
-        char buff[BUFFSIZE];
         myprotobuf.SerializeToArray(buff,BUFFSIZE);
 
-        //客户端发送请求
+        //服务端发送响应
         int len = strlen(buff);
-        zmq_msg_t req;
-        if(0 != zmq_msg_init_size(&req,len))
+        zmq_msg_t reply;
+        if(0 != zmq_msg_init_size(&reply,len)) //zmq_msg_init_size - initialise 0MQ message of a specified size
         {
             cout<<"zmq_msg_init failed..."<<endl;
             break;
         }
-        memcpy(zmq_msg_data(&req),buff,len);
-        if(len != zmq_msg_send(&req,requester,0))
+        memcpy(zmq_msg_data(&reply),buff,len);
+        if(len != zmq_msg_send(&reply,responder,0)) //zmq_msg_send - send a message part on a socket
         {
-            zmq_msg_close(&req);
+            zmq_msg_close(&reply); //release 0MQ message
             cout<<"send faliled..."<<endl;
             break;
         }
@@ -57,24 +73,12 @@ int main()
         cout<<"Type:"<<myprotobuf.type()<<"\t"
             <<"IP:"<<myprotobuf.ip()<<"\t"
             <<"Port:"<<myprotobuf.port()<<"\n";
-        zmq_msg_close(&req);
-
-        //清空发送缓存
-        memset(buff,0,BUFFSIZE*sizeof(char));
-
-        //客户端接收来自服务端的相应
-        zmq_msg_t reply;
-        zmq_msg_init(&reply);
-        int size = zmq_msg_recv(&reply,requester,0);
-        memcpy(buff,zmq_msg_data(&reply),size);
-        Information receive;
-        receive.ParseFromArray(buff,BUFFSIZE);
-        cout<<"Type:"<<receive.type()<<"\t"
-            <<"IP:"<<receive.ip()<<"\t"
-            <<"Port:"<<receive.port()<<"\n";
-        zmq_msg_close(&reply);
+        zmq_msg_close(&reply); //release 0MQ message
     }
-    zmq_close(&requester);
-    zmq_ctx_destroy(context);
+    
+    //最后需要关闭套接字，并销毁上下文
+    zmq_close(&responder); //关闭套接字
+    zmq_ctx_destroy(context); //销毁上下文
     return 0;
 }
+
